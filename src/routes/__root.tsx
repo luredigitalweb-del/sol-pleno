328b27a5af826b1c40e8deaddbdaf6c83e241460
luredigitalweb-del <luredigitalequipe@gmail.com>
2026-06-16 15:39:07 -0300

    Adiciona Google Tag Manager (GTM-M5Q47HMP)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,6 +102,12 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: logoUrl,
       },
     ],
+    scripts: [
+      {
+        // Google Tag Manager
+        children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M5Q47HMP');`,
+      },
+    ],
   }),
   shellComponent: RootShell,
   component: RootComponent,
@@ -116,6 +122,16 @@ function RootShell({ children }: { children: ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M5Q47HMP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="gtm"
+          />
+        </noscript>
         {children}
         <Scripts />
       </body>
